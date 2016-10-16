0f09785336c0acb1a050dcc10cb1e084a96d9c52
StaceyMui <stacey.mui2017@gmail.com>
2016-10-16 13:27:29 -0400

    Peer messaging continued

--- src/RUBTClient.java
+++ src/RUBTClient.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import GivenTools.Bencoder2;
 import GivenTools.BencodingException;
 import GivenTools.TorrentInfo;
@@ -64,7 +63,7 @@ private final int uploaded;
 
 private final int left;*/
 	
-	private static byte[] clientID = generatePeerId();
+	private static byte[] clientID;
 
 	public static void main(String[] args) throws URISyntaxException, IOException, BencodingException, NoSuchAlgorithmException 
 	{
@@ -213,10 +212,10 @@ private final int left;*/
 		HttpURLConnection connection = (HttpURLConnection)key.openConnection();
 		connection.setRequestMethod("GET");
 		//notetoself: change variable names to avoid plagarism
-		DataInputStream dis = new DataInputStream(connection.getInputStream());
+		DataInputStream inputStream = new DataInputStream(connection.getInputStream());
 		contentLength =connection.getContentLength(); //variable changed here
 		response = new byte[contentLength];
-		dis.readFully(response); // Tracker returns object here
+		inputStream.readFully(response); // Tracker returns object here
 		return response;
 	}
 	
@@ -227,6 +226,7 @@ private final int left;*/
 		
 	private static URL makeKey(TorrentInfo ti) throws URISyntaxException, MalformedURLException {
 		byte[] hash = ti.info_hash.array();
+		clientID = generatePeerId();
 		URI uri = ti.announce_url.toURI();
 		String stringKey = uri.getPath() + "?info_hash=";
 		for(byte b : hash)
@@ -251,8 +251,10 @@ private final int left;*/
 	}
 	
 	private static void peerHandshake(Peer p, TorrentInfo ti) throws IOException, BencodingException, NoSuchAlgorithmException {
+		//byte id;
+		int message_length, pieceDesired=0;
 		Socket TCPSocket = new Socket(p.ip, p.port);
-		byte[] reserved = new byte[8], peerHandshake = new byte[68];
+		byte[] reserved = new byte[8], peerHandshake = new byte[68], length = new byte[4];
 		
 		//BufferedReader br = new BufferedReader(new InputStreamReader(TCPSocket.getInputStream()));
 		DataInputStream dis = new DataInputStream(TCPSocket.getInputStream());
@@ -265,33 +267,328 @@ private final int left;*/
 		os.write(reserved, 0, reserved.length);
 		byte[] hash = ti.info_hash.array();
 		os.write(Bencoder2.encode((ByteBuffer.wrap(hash))));
+		System.out.println(new String(clientID));
 		os.write(clientID);
 		
-		dis.readFully(peerHandshake);
+
+		//1+19+8+20+20 bytes in a handshake
+		/*byte[] handshakeMessage = new byte[68];
+		int index = 0;
+		//first byte is 0
+		handshakeMessage[index] = 0x13;
+		index++;
+		//'BitTorrent protocol' string
 		
+		for(int i = 0; i<19; i++){
+			handshakeMessage[index] = (byte) "BitTorrent protocol".charAt(i);
+			index++;
+		}
+		
+		//8 '0's
+		for(int i = 0; i<8; i++){
+			handshakeMessage[index] = 0;
+			index++;
+		}
+		
+		//20-byte torrent info hash
+		for(int i = 0; i<20; i++){
+			handshakeMessage[index] = ti.info_hash.get(i);
+			index++;
+		}
+		
+		//20-byte peer ID
+		for(int i = 0; i<20; i++){
+			handshakeMessage[index] = clientID[i];	
+			index++;
+		}
+
+		/*byte[] handshake = new byte[68];
+		
+		handshake[index] = 0x13;
+		index++;
+		
+		byte[] BTChars = { 'B', 'i', 't', 'T', 'o', 'r', 'r', 'e', 'n', 't', ' ',
+				'p', 'r', 'o', 't', 'o', 'c', 'o', 'l' };
+		System.arraycopy(BTChars, 0, handshake, index, BTChars.length);
+		index += BTChars.length;
+		
+		byte[] zero = new byte[8];
+		System.arraycopy(zero, 0, handshake, index, zero.length);
+		index += zero.length;
+		
+		System.arraycopy(ti.info_hash.array(), 0, handshake, index, ti.info_hash.array().length);
+		index += ti.info_hash.array().length;
+		
+		System.arraycopy(clientID, 0, handshake, index, clientID.length);
+		*/
+		
+		//os.write(handshakeMessage);
+		
+		dis.read(peerHandshake, 0, 68);
+		/*System.out.println("Peer handshake:");
+		for (int i = 0; i < peerHandshake.length; i++)
+		{
+			System.out.println(peerHandshake[i]);
+		}*/
+		
+		/*for(int i = 0; i < 68; i++)
+			System.out.println(peerHandshake[i]);
+		System.out.println("Done reading peerHandshake");
+		for (int i = 0; i < 68; i++ )
+			System.out.println(ti.info_hash.array()[i]);
+		System.out.println(new String(peerHandshake));*/
+		String handshakePeerID = new String (Arrays.copyOfRange(peerHandshake, 48, 68));
+		//System.out.println(handshakePeerID);
+		//System.out.println(peerID);
 		 for(int i = 0; i<20; i++){
-		    	if((char)peerHandshake[i+48] != peerID.charAt(i)){
+			 //System.out.println(handshakePeerID.charAt(i));
+			 //System.out.println(peerID.charAt(i));
+		    	if(handshakePeerID.charAt(i) != peerID.charAt(i)){
 		    		LOGGER.log(Level.SEVERE, "Connected with the wrong peer.");
 		    		TCPSocket.close();
 		    		return;
 		    	}
 		 }
-		
-		 MessageDigest digest = null;
+		 /*MessageDigest digest = null;
 		 digest = MessageDigest.getInstance("SHA-1");
-			
-			digest.update(peerHandshake);
-			byte[] info_hash_of_response = digest.digest();
-		    
-			if (Arrays.equals(info_hash_of_response, ti.info_hash.array())){
-				System.out.println("Peer connection issue: info hashes do not match");
-		    } 
 		 
+		digest.update(peerHandshake);*/
+		//byte[] info_hash_of_response = digest.digest();
+		byte[] info_hash_of_response = Arrays.copyOfRange(peerHandshake, 28, 48);
+		/*for(int i = 0; i<20; i++){
+			System.out.println(info_hash_of_response[i]);
+			System.out.println(ti.info_hash.array()[i]);
+	    	if(info_hash_of_response[i] != ti.info_hash.array()[i]){
+	    		LOGGER.log(Level.SEVERE, "Connected with the wrong peer.");
+	    		TCPSocket.close();
+	    		return;
+	    	}
+	 	}*/
+		if (!Arrays.equals(info_hash_of_response, ti.info_hash.array())){
+			LOGGER.log(Level.SEVERE, "Peer connection issue: info hashes do not match");
+    		TCPSocket.close();
+    		return;
+	    }
+		System.out.println("Info hashes match.");
 		//Sending interested message
-		/*os.writeByte( 1 );
-		os.writeInt( 2 );
-		peerResponse = br.readLine();
-		TCPSocket.close();*/
+		LOGGER.info("Sending Interested message" );
+		
+		/*os.writeByte(1);
+		os.writeInt( 2 );*/
+		byte [] interested = {0, 0, 0, 1, 2};
+		os.write(interested);
+		/*ByteBuffer buf = ByteBuffer.allocate(8); // two 4-byte integers
+		buf.putInt( 1 ).putInt( 2 );
+		buf.flip();
+		byte[] b = buf.array();
+		os.write(b);*/
+		LOGGER.info("Awaiting peer response");
+		/*boolean requestable = false;
+		do {
+			message_length = dis.read(length);
+			System.out.println(message_length);
+			/*if(message_length != 0 && message_length != 1 && message_length != 5 && message_length != 13 ) { 
+				System.out.println("Junk response");
+				continue;
+			}*/
+			/*System.out.println("Message length: " + message_length);
+			if(message_length == 0){
+				System.out.println("keep alive");
+			}
+			id = dis.readByte();
+			System.out.println("id: " + id);
+			else if(message_length == 1){
+				//choking message
+				if(id == 0){
+					System.out.println("peer choked");
+				}
+				//unchoking message
+				else if(id == 1){
+					System.out.println("peer unchoked");
+					requestable = true;
+				}
+			}
+			else{
+				if(id == 5){
+					System.out.println(in.available());
+
+					byte[] bitfieldBytes = new byte[length-1];
+					for(int i = 0; i<bitfieldBytes.length; i++){
+						bitfieldBytes[i] = this.in.readByte();
+					}
+					
+					boolean[] bitfield = convert(bitfieldBytes, Download.torrent.piece_hashes.length);
+					System.out.println("Total number of pieces: " + Download.torrent.piece_hashes.length);
+					int pieceCount = 0;
+					this.bitfield = bitfield;
+					for(int i = 0; i<this.bitfield.length; i++){
+						if(bitfield[i] == true){
+							pieceCount++;
+						}
+					}
+					System.out.println("This peer has " + pieceCount + " of them");
+					System.out.println();
+					out.write(interested);
+				}
+		} while(!requestable);*/
+		
+		
+		boolean chokingUs = true;
+		boolean connected = true;
+		System.out.println("reading messages");
+		while(connected){			
+				message_length = dis.readInt();
+				System.out.println("Read int");
+				if(message_length == 0){
+					System.out.println("keep alive");
+					return;
+				}
+				byte id = dis.readByte();
+				if(message_length == 1){
+					//choking message
+					if(id == 0){
+						System.out.println("choking us");
+						chokingUs = true;
+					}
+					//unchoking message
+					else if(id == 1){
+						System.out.println("no longer choking us");
+						chokingUs = false;
+						//this.getPiece(0);
+					}
+					//interested message
+					else if(id == 2){
+						System.out.println("interested in us");
+					}
+					//not interested message
+					else if(id == 3){
+						System.out.println("not interested in us");
+					}
+				}
+				//have message
+				else if(message_length == 5 && id == 4){
+					System.out.println("have = " + dis.readInt());
+					
+				}
+				else{
+					if(id == 5){
+						System.out.println(dis.available());
+
+						byte[] bitfieldBytes = new byte[message_length-1];
+						for(int i = 0; i<bitfieldBytes.length; i++){
+							bitfieldBytes[i] = dis.readByte();
+						}
+						
+						boolean[] bitfield = convert(bitfieldBytes, ti.piece_hashes.length);
+						System.out.println("Total number of pieces: " + ti.piece_hashes.length);
+						int pieceCount = 0;
+						//this.bitfield = bitfield;
+						for(int i = 0; i<bitfield.length; i++){
+							if(bitfield[i] == true){
+								pieceCount++;
+							}
+						}
+						System.out.println("This peer has " + pieceCount + " of them");
+						System.out.println();
+						os.write(interested);
+					}
+				}
+			System.out.println(dis.available());
+			if(pieceDesired != -1 && !chokingUs){
+				int pieceNumber = 0;
+				int requestLength = 16384;
+				int left = ti.piece_length;
+				byte[] request = new byte[17];
+				
+				request[0] = 0;
+			    request[1] = 0;
+			    request[2] = 0;
+			    request[3] = 13;
+			    request[4] = 6;
+			    
+			    byte[] pNBytes = ByteBuffer.allocate(4).putInt(pieceNumber).array();
+			    //PieceNumberBytes
+			    byte[] offsetBytes, lengthBytes;
+			    request[5] = pNBytes[0]; request[6] = pNBytes[1]; request[7] = pNBytes[2]; request[8] = pNBytes[3];
+			    
+			    byte[] response = new byte[100]; //first subpiece
+				byte[] finalPiece = new byte[ti.piece_length];
+				
+				while(left > 0){
+					
+					if(left > 16384){
+						//make a request for a piece of length 16384
+						requestLength = 16384;
+					}else{
+						//make a request for a piece of length however many bytes are left
+						requestLength = left;
+					}
+					
+					offsetBytes = ByteBuffer.allocate(4).putInt(ti.piece_length - left).array();
+				    
+				    request[9] = offsetBytes[0]; request[10] = offsetBytes[1]; request[11] = offsetBytes[2]; request[12] = offsetBytes[3];
+				    
+				    lengthBytes = ByteBuffer.allocate(4).putInt(requestLength).array();
+				    
+				    request[13] = lengthBytes[0];
+				    request[14] = lengthBytes[1];
+				    request[15] = lengthBytes[2];
+				    request[16] = lengthBytes[3];
+				    
+				    os.write(request);
+				    os.flush();
+				  	
+				  	dis.read(response, 0, 100); // TODO: Make sure it is not a request message!
+				  	if (response == null){
+						System.out.println("Message is null");
+						return;
+					}
+					for(int i = 0; i<response.length; i++){
+						if(0 != 0){
+							if(i<0){
+								System.out.print(String.format("%02x", response[i])+ " ");					
+							}
+							if(i == 0){
+								System.out.print(".....");
+							}
+							if(i>response.length-0){
+								System.out.print(String.format("%02x", response[i])+ " ");
+							}
+						} else{
+							System.out.print(String.format("%02x", response[i])+ " ");
+						}
+					}
+					System.out.println();
+				  	
+				  	for(int i = 0; i < requestLength; i++){
+				  		finalPiece[ti.piece_length - left + i] = response[i + 13];
+				  	}
+				  	
+				    
+					left = left - 16384;
+					response = null;
+				}
+
+			}
+		}	
+		TCPSocket.close();
+	}
+	
+	public static boolean[] convert(byte[] bits, int significantBits) {
+		boolean[] retVal = new boolean[significantBits];
+		int boolIndex = 0;
+		for (int byteIndex = 0; byteIndex < bits.length; ++byteIndex) {
+			for (int bitIndex = 7; bitIndex >= 0; --bitIndex) {
+				if (boolIndex >= significantBits) {
+					// Bad to return within a loop, but it's the easiest way
+					return retVal;
+				}
+
+				retVal[boolIndex++] = (bits[byteIndex] >> bitIndex & 0x01) == 1 ? true
+						: false;
+			}
+		}
+		return retVal;
 	}
 	
 /*private int getDownloaded() 
